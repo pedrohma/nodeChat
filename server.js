e1c9b779b8ee818ebf7285cfb6f5158bf6fc09f1
Pedro Henrique Arthur <pedrohma95@gmail.com>
2018-03-29 19:42:27 -0700

    in and out

--- server.js
+++ server.js
@@ -1,24 +1,53 @@
 var net = require("net");
 
 var count = 0;
+var users = [];
 
 var server = net.createServer(function (conn) {
+
+    var nickname = '';
 
     conn.setEncoding("utf8");
 
     conn.write(
-        "\n > welcome to node-chat" +
+        "\n > welcome to \033[92mnode-chat\033[39m!" +
         "\n > " + count + " other people are connected at this time." +
         "\n > please write your name and press enter: "
     );
     count++;
 
+    function broadcast (msg, exceptMyself) {
+        for (var i in users) {
+          if (!exceptMyself || i != nickname) {
+            users[i].write(msg);
+          }
+      } }
+
     conn.on("data", function (data) {
-        console.log(data);
+        
+
+        data = data.replace("\r\n", "");
+
+        if (!nickname) {
+            if (users[data]) {
+                conn.write("\033[93m> nickname already in use. try again:\033[39m ");
+                return;
+            } else {
+                nickname = data;
+                users[nickname] = conn;
+                broadcast("\033[90m > " + nickname + " joined the room\033[39m\n");
+            }
+        }
+        else {
+            // otherwise you consider it a chat message
+            broadcast("\033[96m > " + nickname + ":\033[39m " + data + "\n", true);
+        }
     });
-    
+
     conn.on("close", function () {
         count--;
+        delete users[nickname];
+        broadcast("\033[90m > " + nickname + " left the room\033[39m\n");
     });
 });
 
