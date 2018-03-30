f77d9de3a0c94f8b2e2189c3fa763557b45e86f8
Pedro Henrique Arthur <pedrohma95@gmail.com>
2018-03-29 19:08:57 -0700

    close connection get messages

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var net = require("net");
 
+var count = 0;
+
 var server = net.createServer(function (conn) {
-    console.log("new Connection!");
+
+    conn.setEncoding("utf8");
+
+    conn.write(
+        "\n > welcome to node-chat" +
+        "\n > " + count + " other people are connected at this time." +
+        "\n > please write your name and press enter: "
+    );
+    count++;
+
+    conn.on("data", function (data) {
+        console.log(data);
+    });
+    
+    conn.on("close", function () {
+        count--;
+    });
 });
 
 server.listen(3000, function () {
