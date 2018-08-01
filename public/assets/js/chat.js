932009c11607008a7a07f67c826c08729f9d8152
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-08-01 12:45:10 -0700

    adding different colors

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -18,7 +18,8 @@ socket.on('message', function (msg) {
     var style = msg.style;
     var message = msg.message;
     var time = msg.date;
-    var from = msg.from;
+    var from = msg.from.name;
+    var color = msg.from.color;
     var appendStr = "";
     switch (style) {
         case 'connected':
@@ -29,10 +30,10 @@ socket.on('message', function (msg) {
             break;
         case "message":
             if (from != null && from != user) {
-                appendStr += "<li class='right clearfix'><div class='chat-body clearfix'><div class='header'><p><strong class='pull-right receiver'><b>" + from + "</b></strong><small class='text-muted'><span class='glyphicon glyphicon-time'></span><i>" + time + "</i></small></p></div><p>" + message + "</p></div></li>";
+                appendStr += "<li class='right clearfix'><div class='chat-body clearfix'><div class='header'><p><strong class='pull-right' style='color: " + color + " '><b>" + from + "</b></strong><small class='text-muted'><span class='glyphicon glyphicon-time'></span><i>" + time + "</i></small></p></div><p>" + message + "</p></div></li>";
             }
             else {
-                appendStr += "<li class='left clearfix'><div class='chat-body clearfix'><div class='header'><p><strong class='pull-right sender'><b>" + from + "</b></strong><small class='text-muted'><span class='glyphicon glyphicon-time'></span><i>" + time + "</i></small></p></div><p>" + message + "</p></div></li>";
+                appendStr += "<li class='left clearfix'><div class='chat-body clearfix'><div class='header'><p><strong class='pull-right' style='color: " + color + "><b>" + from + "</b></strong><small class='text-muted'><span class='glyphicon glyphicon-time'></span><i>" + time + "</i></small></p></div><p>" + message + "</p></div></li>";
             }
             break;
         default:
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.static('./public'));
 
 var users = [];
 
+var colors = ["#d77ee7", "#7ea0e7", "#7ee7bf", "#7ee795", "#c9ff00", "#ffc100", "#8245e3", "#8245e3", "#900303", "#f6b045"];
+
 var user = "";
 
 var listener = app.listen(process.env.PORT || 3000, function () {
@@ -30,25 +32,47 @@ io.on('connection', function(socket){
 
       socket.on('message', function(msg, user){
         if(msg != '' && msg != null){
+          var user = getUser(user);
+          console.log('this is the user -> ' + user.name);
           var message = {style: 'message', message: msg, date: format.data.formatAMPM(new Date()), from: user};
-          console.log(format.data.formatAMPM(new Date()));
+          // console.log(format.data.formatAMPM(new Date()));
           io.emit('message', message);
-          console.log('user ' + user + ' sent : ' + msg);
-          db.newMsg(message);
+          // console.log('user ' + user + ' sent : ' + msg);
+          // db.newMsg(message);
         }
       });
 
       socket.on('checkUser', function (user){
+        var color = getColor();
+        var user = {name: user, color: color};
         users.push(user);
-        var msg = {style: 'connected', message: user + " connected", date: format.data.formatAMPM(new Date()), from: null};
+        console.log(user);
+        var msg = {style: 'connected', message: user.name + " connected", date: format.data.formatAMPM(new Date()), from: null, color: color};
         socket.broadcast.emit('message', msg);
-        db.newUser(msg);
+        // db.newUser(msg);
       });
 
       socket.on('typing', function(user){
-        var msg = {style: 'connected', message: user + " is typing a message...", date: format.data.formatAMPM(new Date()), from: null};
+        var msg = {style: 'connected', message: user.name + " is typing a message...", date: format.data.formatAMPM(new Date()), from: null};
         socket.broadcast.emit('typing', msg);
     });
 
 });
 
+function getColor(){
+  var num = Math.floor((Math.random() * colors.length) + 1);
+  var color = colors[num];
+  var index = colors.indexOf(num);
+  colors.splice(index, 1);
+  return color;
+}
+
+function getUser(name){
+  var user = null;
+  for(i = 0; i < users.length; i++){
+    if(users[i].name == name){
+      user = users[i];
+    }
+  }
+  return user;
+}
