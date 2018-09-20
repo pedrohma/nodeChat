b6afbc6b8579f0053826e00da36a72be370fe400
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-09-20 14:05:56 -0700

    playing sounds

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -19,28 +19,33 @@ socket.on('message', function (msg) {
     var message = msg.message;
     var time = msg.date;
     var appendStr = "";
+    var audio;
     switch (style) {
         case 'connected':
             appendStr += "<div class='alert alert-success' role='alert'>" + message + "</div>"
+            audio =  new Audio('/assets/sound/new.mp3');
             break;
         case 'disconnect':
             appendStr += "<div class='alert alert-danger' role='alert'>" + message + "</div>"
+            audio =  new Audio('/assets/sound/out.mp3');
             break;
         case "message":
             var from = msg.from.name;
             var color = msg.from.color;
             if (from != null && from != user) {
                 appendStr += "<li class='right clearfix'><div class='chat-body clearfix'><div class='header'><p><strong class='pull-right' style='color: " + color + " '><b>" + from + "</b></strong><small class='text-muted'><span class='glyphicon glyphicon-time'></span><i>" + time + "</i></small></p></div><p>" + message + "</p></div></li>";
+                audio =  new Audio('/assets/sound/stairs.mp3');
             }
             else {
                 appendStr += "<li class='left clearfix'><div class='chat-body clearfix'><div class='header'><p><strong class='pull-right' style='color: " + color + "><b>" + from + "</b></strong><small class='text-muted'><span class='glyphicon glyphicon-time'></span><i>" + time + "</i></small></p></div><p>" + message + "</p></div></li>";
+                audio =  new Audio('/assets/sound/stairs.mp3');
             }
             break;
         default:
-
             break;
     }
-
+    
+    audio.play();
     $('.chat').append($(appendStr));
 });
 
